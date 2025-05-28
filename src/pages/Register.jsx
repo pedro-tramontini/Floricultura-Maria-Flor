68d05b5cbf8babfe1355088616b578cfe67eb255
Pedro Tramontini <pedroh.tramontini@gmail.com>
2025-05-28 14:51:42 -0300

    feat: Salvamento dos dados de registro dentro da fakeApi

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,37 +3,81 @@ import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import FormControl from '@mui/material/FormControl';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
 
-    const handleSubmit = async (evento) => {
-        evento.preventDefault();
 
-        const data = new FormData(evento.currentTarget)
-        const nome = data.get('nome')
-        const ddd = data.get('ddd')
-        const telefone = data.get('telefone')
-        const email = data.get('email')
-        const rua = data.get('rua')
-        const número = data.get('número')
-        const bairro = data.get('bairro')
-        const cep = data.get('cep')
-
-        const send = {
-            nome: nome,
-            ddd: ddd,
-            telefone: telefone,
-            email: email,
-            rua: rua,
-            número: número,
-            bairro: bairro,
-            cep: cep,
-        }
-
-        console.log(send)
-    }
 
 
 export default function Register() {
     const navigate = useNavigate()
+    const [errorNome, setErrorNome] = useState(false)
+    const [errorDdd, setErrorDdd] = useState(false)
+    const [errorTelefone, setErrorTelefone] = useState(false)
+    const [errorEmail, setErrorEmail] = useState(false)
+    const [errorRua, setErrorRua] = useState(false)
+    const [errorNumero, setErrorNumero] = useState(false)
+    const [errorBairro, setErrorBairro] = useState(false)
+    const [errorCep, setErrorCep] = useState(false)
+    const [flowerCadasterObj, setFlowerCadasterObj] = useState([])
+
+    
+    const handleSubmit = async (evento) => {
+        evento.preventDefault()
+        setErrorNome()
+        setErrorDdd()
+        setErrorTelefone()
+        setErrorEmail()
+        setErrorRua()
+        setErrorNumero()
+        setErrorBairro()
+        setErrorCep()
+        const data = new FormData(evento.currentTarget)
+
+        if (data.get('nome') == "") {
+                setErrorNome(true)
+            }
+            
+        if (data.get('ddd') == "") {
+                setErrorDdd(true)
+            }
+            
+        if (data.get('telefone') == "") {
+                setErrorTelefone(true)
+            }
+            
+        if (data.get('email') == "") {
+                setErrorEmail(true)
+            }
+        if (data.get('rua') == "") {
+                setErrorRua(true)
+            }
+            
+        if (data.get('número') == "") {
+                setErrorNumero(true)
+            }
+        if (data.get('bairro') == "") {
+                setErrorBairro(true)
+            }
+            
+        if (data.get('cep') == "") {
+                setErrorCep(true)
+            }
+        
+        if (data.get('nome') && data.get('ddd') && data.get('telefone') && data.get('email') && data.get('rua') && data.get('número') && data.get('bairro') && data.get('cep')) {
+            axios.post('http://localhost:8001/Cadasters', {
+                "nome": data.get('nome'),
+                "ddd": data.get('ddd'),
+                "telefone": data.get('telefone'),
+                "email": data.get('email'),
+                "rua": data.get('rua'),
+                "número": data.get('numero'),
+                "bairro": data.get('bairro'),
+                "cep": data.get('cep'),
+            })
+            location.reload()
+        }
+    }
 
 
 
@@ -44,7 +88,7 @@ export default function Register() {
             <Container sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}} >
                 <Box sx={{boxShadow: 10, margin: '5%',display: 'flex', justifyContent: 'center', padding: 2 ,backgroundColor: '#E6EBEC', height: '100%', width: 480, borderRadius: 2}}>
                     <Box sx={{padding: 1}}>
-                        <form onSubmit={handleSubmit} id='form-new'>
+                        <form noValidate onSubmit={handleSubmit} id='form-new'>
                             <FormControl>
                                 <Typography variant='h5' color='terciary' sx={{paddingBlock: 1}}>Cadastro Cliente</Typography>
                             </FormControl>
@@ -52,7 +96,8 @@ export default function Register() {
                             <FormControl variant='filled' sx={{width: '100%'}} >
                                 <Typography variant='body1' color='terciary' sx={{paddingBlock: 1}}>Nome</Typography>
                                 <TextField
-                                    name='nome'           
+                                    name='nome'
+                                    error={errorNome}           
                                     slotProps={{htmlInput: { maxLength: 60 }}}                         
                                     required
                                     variant='filled'
@@ -73,6 +118,7 @@ export default function Register() {
                                     <FormControl variant='filled' sx={{width: '100%'}} >
                                         <TextField
                                             name='ddd'
+                                            error={errorDdd}
                                             slotProps={{htmlInput: { maxLength: 3 }}}
                                             required
                                             variant='filled'
@@ -87,6 +133,7 @@ export default function Register() {
                                     <FormControl variant='filled' sx={{width: '100%'}} >
                                         <TextField
                                             name='telefone'
+                                            error={errorTelefone}
                                             slotProps={{htmlInput: { maxLength: 11, minLength: 11 }}}
                                             required
                                             variant='filled'
@@ -108,6 +155,7 @@ export default function Register() {
                                 <TextField
                                     slotProps={{htmlInput: { maxLength: 60 }}}
                                     name='email'
+                                    error={errorEmail}
                                     required
                                     variant='filled'
                                     color='terciary'
@@ -128,6 +176,7 @@ export default function Register() {
                                     <TextField
                                         slotProps={{htmlInput: { maxLength: 60 }}}
                                         name='rua'
+                                        error={errorRua}
                                         required
                                         variant='filled'
                                         color='terciary'
@@ -142,6 +191,7 @@ export default function Register() {
                                     <TextField
                                         slotProps={{htmlInput: { maxLength: 10 }}}
                                         name='número'
+                                        error={errorNumero}
                                         required
                                         variant='filled'
                                         color='terciary'
@@ -156,6 +206,7 @@ export default function Register() {
                                     <TextField
                                         slotProps={{htmlInput: { maxLength: 60 }}}
                                         name='bairro'
+                                        error={errorBairro}
                                         required
                                         variant='filled'
                                         color='terciary'
@@ -169,6 +220,7 @@ export default function Register() {
                                     <FormControl variant='filled' sx={{width: '100%'}} >
                                     <TextField
                                         name='cep'
+                                        error={errorCep}
                                         slotProps={{htmlInput: { maxLength: 8, minLength: 8 }}}
                                         required
                                         variant='filled'
@@ -182,7 +234,8 @@ export default function Register() {
                             </Grid>
 
                             <FormControl sx={{width: '100%', paddingBlock: 2}}>
-                                <Button onClick={() =>navigate('/login')} type='submit' variant="contained" color='secondary' sx={{borderRadius: 0}}>Cadastrar</Button>
+                                <Button  type='submit' variant="contained" color='secondary' sx={{borderRadius: 0}}>Cadastrar</Button>
+                                {/* onClick={() =>navigate('/login')} */}
                             </FormControl>
 
                                 <Divider sx={{paddingBottom: 2}}>
@@ -192,7 +245,8 @@ export default function Register() {
                             <FormControl sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'row'}}>
                                 <Typography variant='body1' color='terciary.ligth' sx={{marginRight: .5}}>Já possui uma conta?</Typography>
                                 <Link href='#' color='terciary.ligth'>
-                                <Typography onClick={() =>navigate('/login')} variant='body1' color='terciary.ligth'>Faça login</Typography>
+                                <Typography variant='body1' color='terciary.ligth'>Faça login</Typography>
+                                {/* onClick={() =>navigate('/login')} */}
                                 </Link>
                             </FormControl>
                         </form>
